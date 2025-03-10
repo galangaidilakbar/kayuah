244870b7d3b12e15bece026f3e3c65c2e18666fd
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-10 09:30:40 +0700

    participant
    event boat participant

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
@@ -14,5 +15,10 @@ class Event extends Model
     public function venue(): BelongsTo
     {
         return $this->belongsTo(Venue::class);
+    }
+
+    public function participants(): HasMany
+    {
+        return $this->hasMany(Participant::class);
     }
 }
--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -5,20 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Boat extends Model
+class Participant extends Model
 {
     use HasUuids, SoftDeletes;
 
-    public function village(): BelongsTo
+    public function event(): BelongsTo
     {
-        return $this->belongsTo(Village::class);
+        return $this->belongsTo(Event::class);
     }
 
-    public function participants(): HasMany
+    public function boat(): BelongsTo
     {
-        return $this->hasMany(Participant::class);
+        return $this->belongsTo(Boat::class);
     }
 }
