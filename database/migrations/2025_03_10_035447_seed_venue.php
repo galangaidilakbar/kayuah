9ee1fff90781a8b90c64fa767180fd5b75578e08
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-10 10:58:44 +0700

    seed venue

---
+++ database/migrations/2025_03_10_035447_seed_venue.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\SubDistrict;
+use App\Models\Venue;
+use Illuminate\Database\Migrations\Migration;
+use League\Csv\Reader;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $csv = Reader::createFromPath(database_path('data/venues.csv'));
+        $csv->setHeaderOffset(0);
+
+        foreach ($csv as $record) {
+            $sub_district = SubDistrict::whereCode($record['sub_district_id'])->first();
+
+            if ($sub_district) {
+                Venue::firstOrCreate([
+                    'sub_district_id' => $sub_district->id,
+                    'name' => $record['name'],
+                ], []);
+            }
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('venues')->delete();
+    }
+};
