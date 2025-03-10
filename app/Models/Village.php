d97aed9aa40753f07b6a515b139fac92bea4b152
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-10 09:11:56 +0700

    village

---
+++ app/Models/Village.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Village extends Model
+{
+    use HasUuids, SoftDeletes;
+
+    public function subDistrict(): BelongsTo
+    {
+        return $this->belongsTo(SubDistrict::class);
+    }
+}
