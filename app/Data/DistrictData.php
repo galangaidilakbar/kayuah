2034e65223bb86bfe9f0f27050c610a352c4ac11
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-13 14:17:51 +0700

    district data

---
+++ app/Data/DistrictData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+
+class DistrictData extends Data
+{
+    public function __construct(public string $id, public ?string $code, public string $name) {}
+}
