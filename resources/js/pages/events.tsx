20ea895b01e2620fd7693dfaad6e0f91e0431ddf
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-14 23:32:43 +0700

    timezone asia jakarta

--- resources/js/pages/events.tsx
+++ resources/js/pages/events.tsx
@@ -2,22 +2,32 @@ import { Head, usePage } from '@inertiajs/react';
 
 export default function Events() {
     const { props } = usePage<{ events: App.Data.EventData[] }>();
-
     const { events } = props;
-
+    
+    // Function to format the date
+    const formatDate = (dateString: string) => {
+        const date = new Date(dateString);
+        return new Intl.DateTimeFormat('id-ID', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true
+        }).format(date);
+    };
+    
     return (
         <div className="space-y-6 p-8">
-            <Head title="Districts" />
-
+            <Head title="Events" />
             <h1 className="text-2xl font-bold">Events</h1>
-
             <div>
                 <ul className="space-y-6">
                     {events.map((event) => (
                         <li key={event.id}>
                             <div className="text-xl">{event.name}</div>
-                            <div className="text-sm">{event.start_date}</div>
-                            <div className="text-sm">{event.end_date}</div>
+                            <div className="text-sm">Start: {formatDate(event.start_date)}</div>
+                            <div className="text-sm">End: {formatDate(event.end_date)}</div>
                         </li>
                     ))}
                 </ul>
