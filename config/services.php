14ab1d9a2b81f9038e9964c475715b9a86c2788c
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-03-27 19:25:47 +0700

    WIP

--- config/services.php
+++ config/services.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Third Party Services
@@ -35,4 +34,9 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'app_id' => env('FACEBOOK_APP_ID'),
+        'app_secret' => env('FACEBOOK_APP_SECRET'),
+        'redirect_uri' => env('FACEBOOK_REDIRECT_URI'),
+    ],
 ];
