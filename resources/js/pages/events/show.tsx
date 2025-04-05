55e568bfe0b1e38a5071512adb0fe26ade858b98
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-04-05 11:38:08 +0700

    WIP

--- resources/js/pages/events/show.tsx
+++ resources/js/pages/events/show.tsx
@@ -2,10 +2,24 @@ interface ShowProps {
     event: App.Data.EventData;
 }
 
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Events',
+        href: '/dashboard',
+    },
+];
+
 export default function Show({ event }: ShowProps) {
     return (
-        <div>
-            <h1>{event.name}</h1>
-        </div>
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title={event.name} />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                <h1>{event.name}</h1>
+            </div>
+        </AppLayout>
     );
 }
