1b64f4b5aa5911b265c5bc48bdecdb757582e788
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-04-06 16:08:11 +0700

    WIP

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Geist')
             ->sidebarCollapsibleOnDesktop()
+            ->navigationGroups([
+                __('Schedule'),
+                __('Participants'),
+                __('Area')
+            ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
