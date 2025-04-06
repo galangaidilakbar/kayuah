2ea5e43787c2becf85c9f5fe31363da08d87f8a5
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-04-06 20:54:09 +0700

    composer require secondnetwork/blade-tabler-icons

--- app/Filament/Resources/BoatResource.php
+++ app/Filament/Resources/BoatResource.php
@@ -16,7 +16,7 @@ class BoatResource extends Resource
 {
     protected static ?string $model = Boat::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'tabler-kayak';
 
     public static function getNavigationLabel(): string
     {
