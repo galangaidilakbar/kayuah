6346375f83c7afebee3e4c1dbb03ce443f6d8698
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-04-09 20:23:16 +0700

    expect new users should have role visitor

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Enums\Role as EnumsRole;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('registration screen can be rendered', function () {
@@ -9,6 +13,8 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+    Role::create(['name' => EnumsRole::visitor->value]);
+
     $response = $this->post('/register', [
         'name' => 'Test User',
         'email' => 'test@example.com',
@@ -18,4 +24,20 @@ test('new users can register', function () {
 
     $this->assertAuthenticated();
     $response->assertRedirect(route('dashboard', absolute: false));
+});
+
+test('new users should have role visitor', function () {
+    Role::create(['name' => EnumsRole::visitor->value]);
+
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $user = User::whereEmail('test@example.com')->first();
+    $this->assertAuthenticated();
+    $response->assertRedirect(route('dashboard', absolute: false));
+    expect($user->hasRole(EnumsRole::visitor->value))->toBeTrue();
 });
