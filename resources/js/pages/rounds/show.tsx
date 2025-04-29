4b1c116d31bad7d58bc636fdd8211acdef2fcc09
Galang Aidil Akbar <galangaidil45@gmail.com>
2025-04-30 00:13:12 +0700

    WIP

--- resources/js/pages/rounds/show.tsx
+++ resources/js/pages/rounds/show.tsx
@@ -1,2 +1,35 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
 
+interface ShowProps {
+    round: App.Data.RoundData;
+    races: App.Data.RaceData[];
+}
 
+export default function Show({ round, races }: ShowProps) {
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: 'Events',
+            href: route('events.index')
+        },
+        {
+            title: round.day?.event?.name || 'Event Detail',
+            href: route('events.show', round.day?.event_id)
+        },
+        {
+            title: round.day?.name + ' - ' + round.name,
+            href: route('rounds.show', round.id)
+        }
+    ];
+
+    return (
+
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                {round.name}
+            </div>
+        </AppLayout>
+    );
+}
